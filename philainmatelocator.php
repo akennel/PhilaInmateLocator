97e4d06303e3f67dd3b25c515a013877f9e327f5
Andrew Kennel <andrew.kennel@phila.gov>
2014-06-27 14:33:54 -0400

    Updated URL. Show/Hide result and search section

--- philainmatelocator.php
+++ philainmatelocator.php
@@ -11,33 +11,30 @@ add_shortcode('PhilaInmateLocator', 'philainmatelocator_handler');
 function philainmatelocator_handler(){
 $message = <<<EOM
 
-<html>
 <head>
 <script type="text/javascript">
 <!--
 function ClearSearchCriteria() {
 	document.getElementById("txtLName").value = "";
 	document.getElementById("txtFName").value = "";
-	document.getElementById("txtDob").value = "";
 	document.getElementById("txtppn").value = "";
-	document.getElementById("PhilaInmateResults").innerHTML = "";
+	document.getElementById("PhilaInmateResultsBlock").innerHTML = "";
 }
 //-->
 </script>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+//<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script type="text/javascript">
 <!--
 function validateInmateLocatorFields()
     {
 	var isValid = true;
-        if (document.getElementById("txtppn").value == '' && document.getElementById("txtLName").value == '' && document.getElementById("txtFName").value == '' && document.getElementById("txtDob").value == '')
+        if (document.getElementById("txtppn").value == '' && document.getElementById("txtLName").value == '' && document.getElementById("txtFName").value == '')
         {
             alert("Please Enter Valid Criteria");
             document.getElementById("txtppn").focus();
             isValid = false;
         }
-        if(document.getElementById("txtDob").value == '')
-        {
+
             if ((document.getElementById("txtLName").value != '' && document.getElementById("txtFName").value == ''))
             {
                 alert("Please Enter First Name.");
@@ -50,28 +47,7 @@ function validateInmateLocatorFields()
                 document.getElementById("txtLName").focus();
                 isValid = false;
             }
-        }
-        if(document.getElementById("txtDob").value != '')
-        {
-            if ((document.getElementById("txtLName").value == '' && document.getElementById("txtFName").value == ''))
-            {
-                alert("Please Enter First Name or Last Name.");
-                document.getElementById("txtFName").focus();
-                isValid = false;
-            }
-            if ((document.getElementById("txtLName").value != '' && document.getElementById("txtFName").value == ''))
-            {
-                alert("Please Enter First Name.");
-                document.getElementById("txtFName").focus();
-                isValid = false;
-            }
-            if ((document.getElementById("txtLName").value == '' && document.getElementById("txtFName").value != ''))
-            {
-                alert("Please Enter Last Name.");
-                document.getElementById("txtLName").focus();
-                isValid = false;
-            }
-        }
+      
 		return isValid;
     }
 function startInmateSearch()
@@ -92,22 +68,62 @@ function getInmates(searchCriteria)
 var results = "";
 var requestURL = "http://moisappprd01.phila.gov/PrisonInmateLocator/InmateLocator.asmx/GetListOfInmates?WhereClause=" + searchCriteria;
 //requestURL = "http://localhost/GetListOfInmates.xml";
-var resultsCount = 0;
 var newHTML = "<h2>Found the Following</h2>";
 newHTML += "<ul>";
 
 $.get(requestURL, {}, function(xml){
 	$('DataSet', xml).each(function(read){
 		
+		
 		$('Table', xml).each(function(read){
-			resultsCount = resultsCount + 1;
+		var prisonURL = "";
+		var facilityCode = $(this).find("FACILITY").text().trim();
+		switch(facilityCode) {
+			case "ASDCU":
+			prisonURL = "http://moiswebprd01.phila.gov/prisons/alternative_and_special_detention.htm";
+			break;
+			
+			case "CFCF":
+			prisonURL = "http://moiswebprd01.phila.gov/prisons/curran_fromhold.htm";
+			break;
+			
+			case "DC":
+			prisonURL = "http://moiswebprd01.phila.gov/prisons/detention_center.htm";
+			break;
+		
+			case "HOC":
+			prisonURL = "http://moiswebprd01.phila.gov/prisons/house_of_corrections.htm";
+			break;
+		
+			case "PICC":
+			prisonURL = "http://moiswebprd01.phila.gov/prisons/industrial_correctional_center.htm";
+			break;
+			
+			case "RCF":
+			prisonURL = "http://moiswebprd01.phila.gov/prisons/riverside_correctional_facility.htm";
+			break;
+			
+			case "OJ":
+			prisonURL = "http://moiswebprd01.phila.gov/prisons/oj.htm";
+			break;
+			
+			default:
+			prisonURL = "http://www.phila.gov/prisons";
+			break;
+		}
+		
 			newHTML += "<li>";
-			newHTML += "Facility: " + $(this).find("FACILITY").text() + " | " + $(this).find("PPN").text() + " " ;
+			newHTML += "Name: " + $(this).find("FIRST_NAME").text() + $(this).find("LAST_NAME").text() + " | DOB: " + $(this).find("DOB").text() + "<br/>";
+			newHTML += "PPN: "  + $(this).find("PPN").text() + " | Facility: <a href=" + prisonURL + ">" + $(this).find("FACILITY").text() + "</a>";
 			newHTML += "</li>";
+			
 		});
 		newHTML += "</ul>";
-		newHTML += "Found " + resultsCount + " results.";
-		document.getElementById("PhilaInmateResults").innerHTML = newHTML;
+		
+		newHTML += "<input type=\"submit\" name=\"btnSearchAgain\" value=\"Search Again\" onclick=\"StartNewSearch()\" id=\"btnSearchAgain\">"
+		document.getElementById("PhilaInmateResultsBlock").innerHTML = newHTML;
+		document.getElementById("PhilaInmateSearchBlock").style.display = "none";
+		document.getElementById("PhilaInmateResultsBlock").style.display = "block";
 	});
 });
 
@@ -121,7 +137,6 @@ function buildInmateLocatorSearchCriteria()
 var PPN = document.getElementById("txtppn").value;
 var LName = document.getElementById("txtLName").value;
 var FName = document.getElementById("txtFName").value;
-var DOB = document.getElementById("txtDob").value;
 var sbParameter = "";
 
 	if (PPN != "")
@@ -148,18 +163,18 @@ var sbParameter = "";
         {
             sbParameter += (" First_Name = '" + FName.toUpperCase().trim() + "'");
         }
-        if (DOB != "" && sbParameter.length > 0)
-        {
-            sbParameter += (" and DOB = '" + DOB.toUpperCase().trim() + "'");
-        }
-        else if (DOB != "")
-        {
-            sbParameter += (" DOB = '" + DOB.toUpperCase().trim() + "'");
-        }
         return sbParameter;
 
 }
 
+
+function StartNewSearch()
+{
+
+ClearSearchCriteria();
+document.getElementById("PhilaInmateSearchBlock").style.display = "block";
+document.getElementById("PhilaInmateResultsBlock").style.display = "none";
+}
 
 
 //-->
@@ -176,18 +191,21 @@ var sbParameter = "";
 <div id="PhilaInmateLocatorWidget" class ="PhilaWidget">
 	<span id="PhilaInmateLocatorMainWindow">
 		<h1>Inmate Locator</h1>	
-		PPN:<input name="txtppn" type="text" id="txtppn">
-		<br/>
-		Last Name:<input name="txtLName" type="text" id="txtLName">
-		<br/>
-		First Name:<input name="txtFName" type="text" id="txtFName">
-		<br/>
-		Date of Birth:<input name="txtDob" type="text" id="txtDob">
-		<br/>
-		<div id="PhilaInmateResults"></div>
-		<input type="submit" name="btnInmateLocator" value="Find Inmate" onclick="startInmateSearch();" id="btnInmateLocator"><input type="submit" name="btnClear" value="Clear" onclick="ClearSearchCriteria();" id="btnClear">
+		<div id="PhilaInmateSearchBlock">
+			PPN:<input name="txtppn" type="text" id="txtppn">
+			<br/>
+			Last Name:<input name="txtLName" type="text" id="txtLName">
+			<br/>
+			First Name:<input name="txtFName" type="text" id="txtFName">
+			<br/>
+			<input type="submit" name="btnInmateLocator" value="Find Inmate" onclick="startInmateSearch();" id="btnInmateLocator">
+		</div>
+		<div id="PhilaInmateResultsBlock" style="display:none;">
+			<input type="submit" name="btnSearchAgain" value="Search Again" onclick="StartNewSearch();" id="btnSearchAgain">
+		</div>
+		
 	</span>
-</div> 
+</div>
 
 EOM;
 
